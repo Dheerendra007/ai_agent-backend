d00261fb73a9f96e8154b4501cb6c5911580d9a9
Dheerendra Kapariya <dkapariya@gmail.com>
2026-08-20 23:44:56 +0530

    add new api

--- src/main/java/com/substring/agent/backend/ai_agent_backend/controller/WelcomeController.java
+++ src/main/java/com/substring/agent/backend/ai_agent_backend/controller/WelcomeController.java
@@ -4,6 +4,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -11,8 +13,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WelcomeController {
 
  @GetMapping
-  public ResponseEntity<String> welcome() {
+  public ResponseEntity<String> welcome() {s
     return ResponseEntity.ok("Welcome to the AI Agent Backend!");
   }
+  
+  @GetMapping("path")
+  public String getMethodName(@RequestParam String param) {
+      return new String("Hello, " + param + "! This is the path method.s");
+  }
+  
 }
 
