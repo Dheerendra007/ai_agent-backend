63a3f60f207a02db24bdc672987711dab10c3c82
Dheerendra Kapariya <dkapariya@gmail.com>
2026-08-26 23:59:18 +0530

    update

--- src/main/java/com/substring/agent/backend/ai_agent_backend/controller/ProductDetailsController.java
+++ src/main/java/com/substring/agent/backend/ai_agent_backend/controller/ProductDetailsController.java
@@ -1,5 +1,17 @@
 package com.substring.agent.backend.ai_agent_backend.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
 public class ProductDetailsController {
+
+    @GetMapping("path")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
 
 }
