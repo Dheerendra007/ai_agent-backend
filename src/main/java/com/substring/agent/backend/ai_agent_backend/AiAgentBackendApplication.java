1ad68d67cdb5c71e5b0787878324d7863a6fcfec
Dheerendra Kapariya <dkapariya@gmail.com>
2026-09-10 09:22:10 +0530

    Configure Done

--- src/main/java/com/substring/agent/backend/ai_agent_backend/AiAgentBackendApplication.java
+++ src/main/java/com/substring/agent/backend/ai_agent_backend/AiAgentBackendApplication.java
@@ -8,6 +8,7 @@ public class AiAgentBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiAgentBackendApplication.class, args);
+		System.out.println("AI Agent Backend Application started successfully.");
 	}
 
 }
