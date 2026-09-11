2e8de8ca86a5682da6c0bc3c789e46279e13c4ec
Dheerendra Kapariya <dkapariya@gmail.com>
2026-09-11 22:18:09 +0530

    Added AGENT AI

---
+++ src/main/java/com/substring/agent/backend/ai_agent_backend/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.substring.agent.backend.ai_agent_backend.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping("/api/v1/")
+public class ChatController {
+
+    @Autowired 
+    private ChatClient chatClient;
+
+    
+    @GetMapping("chat")
+    public ResponseEntity<String> chat(@RequestParam (value = "q", required = true) 
+    String q) {
+        // Here you can implement the logic to handle the chat message and generate a response.
+        // For demonstration purposes, we'll just echo back the received message.
+        
+        return ResponseEntity.ok("This is working fine. You sent: " + q);
+    }
+    
+}
